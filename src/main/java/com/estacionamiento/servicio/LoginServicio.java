33d0fabd6d7625dab5d25a2d4c1e269c278c88a3
SaoriPLh <saojym12@gmail.com>
2026-04-24 16:31:00 -0600

    Ahora servicio con objetos

--- src/main/java/com/estacionamiento/servicio/LoginServicio.java
+++ src/main/java/com/estacionamiento/servicio/LoginServicio.java
@@ -32,7 +32,7 @@ public class LoginServicio {
         }
 
         
-        if (p.getIdRol() == MisConstantes.ROL_ADMIN) {
+        if (p.getRol().getIdRol()== MisConstantes.ROL_ADMIN) {
             System.out.println("Acceso total al Administrador.");
             return estacionamientoDAO.listarTodo(); 
         } 
