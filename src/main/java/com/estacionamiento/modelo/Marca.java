50e556a8075c6815de3e5383ea4ce88c00818723
SaoriPLh <saojym12@gmail.com>
2026-04-28 08:37:44 -0600

    Nueva clase marca para evitar redundancia en la bd

---
+++ src/main/java/com/estacionamiento/modelo/Marca.java
@@ -0,0 +1,34 @@
+package com.estacionamiento.modelo;
+
+public class Marca {
+    private int idMarca;
+    private String nombre;
+
+   
+    public Marca() {
+    }
+
+   
+    public Marca(int idMarca, String nombre) {
+        this.idMarca = idMarca;
+        this.nombre = nombre;
+    }
+
+    
+    public int getIdMarca() {
+        return idMarca;
+    }
+
+    public void setIdMarca(int idMarca) {
+        this.idMarca = idMarca;
+    }
+
+   
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
